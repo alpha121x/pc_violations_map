7d0d97483e489d4d45d041645ff7e9fec6a543fb
Abbasch <109143082+alpha121x@users.noreply.github.com>
2026-02-25 11:24:00 +0500

    Update db_config.php

--- services/db_config.php
+++ services/db_config.php
@@ -1,30 +1,16 @@
 <?php
 
-<<<<<<< HEAD
-// $host = "localhost";
-// $port = "5433";
-// $dbname = "db_cnw_rams";   // make sure this database exists
-// $user = "postgres";
-// $password = "1234";        // use the SAME password you used in pgAdmin
-
-
-=======
->>>>>>> parent of 7754b38 (Update db_config.php)
 $host = "172.20.82.84";
 $db   = "price_control_punjab";
 $user = "postgres";
 $pass = "diamondx";
 $port = "5432";
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of 7754b38 (Update db_config.php)
 
 try {
 
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
 
-    $conn = new PDO($dsn, $user, $password, [
+    $conn = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
