5bc0fcb1ddb48d2b6cf5c6323a69cf395437ec18
Abbasch <109143082+alpha121x@users.noreply.github.com>
2026-03-04 12:46:00 +0500

    Update index.php

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://js.arcgis.com/4.29/"></script>
-    <script src="assets/js/index.js"></script>
+    <script src="assets/js/index.js?v=<?= filemtime(__DIR__ . '/assets/js/index.js') ?>"></script>
 
 </body>
 
